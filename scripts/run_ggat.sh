36e07cd096e6ff0fd2ee41697e5191ed17452ac2
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2021-10-31 08:25:40 +0900

    add run script

--- scripts/run_ggat.sh
+++ scripts/run_ggat.sh
@@ -18,7 +18,7 @@ python3 gnn/train.py \
     --no_visdom_logger \
     --loss_name mce \
     --task_type multi_label_node_classification \
-    --network_name GGATUNet \
+    --network_name GGAT \
     --dataset_name PPI \
     --train_transform_name indegree \
     --test_transform_name indegree \
@@ -26,15 +26,14 @@ python3 gnn/train.py \
     --n_epochs 500 \
     --data_dir ./inputs/PPI/ \
     --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name ggat1unet_with_gat_ppi \
+    --name ggat1_with_gat_ppi \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_ggat1unet_with_gat_ppi \
+    --run_name debug_ggat1_with_gat_ppi \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --pool_type SAGPool \
     --ggat_type GGAT1 \
     --gnn_type GAT &
 sleep 10
@@ -44,7 +43,7 @@ python3 gnn/train.py \
     --no_visdom_logger \
     --loss_name mce \
     --task_type multi_label_node_classification \
-    --network_name GGATUNet \
+    --network_name GGAT \
     --dataset_name PPI \
     --train_transform_name indegree \
     --test_transform_name indegree \
@@ -52,17 +51,16 @@ python3 gnn/train.py \
     --n_epochs 500 \
     --data_dir ./inputs/PPI/ \
     --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name ggat1unet_with_gcn_ppi \
+    --name ggat1_with_gcn_ppi \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_ggat1unet_with_gcn_ppi \
+    --run_name debug_ggat1_with_gcn_ppi \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --pool_type SAGPool \
     --ggat_type GGAT1 \
-    --gnn_type GCN
+    --gnn_type GCN &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 2 \
@@ -70,7 +68,7 @@ python3 gnn/train.py \
     --no_visdom_logger \
     --loss_name mce \
     --task_type multi_label_node_classification \
-    --network_name GGATUNet \
+    --network_name GGAT \
     --dataset_name PPI \
     --train_transform_name indegree \
     --test_transform_name indegree \
@@ -78,140 +76,138 @@ python3 gnn/train.py \
     --n_epochs 500 \
     --data_dir ./inputs/PPI/ \
     --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name ggat2unet_with_gat_ppi \
+    --name ggat2_with_gat_ppi \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_ggat2unet_with_gat_ppi \
+    --run_name debug_ggat2_with_gat_ppi \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --pool_type SAGPool \
     --ggat_type GGAT2 \
-    --gnn_type GAT
+    --gnn_type GAT &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 3 \
     --verbose \
     --no_visdom_logger \
     --loss_name mce \
-    --task_type multi_label_node_classification \
-    --network_name GGATUNet \
-    --dataset_name PPI \
+    --task_type graph_classification \
+    --network_name GGAT \
+    --dataset_name MUTAG \
     --train_transform_name indegree \
     --test_transform_name indegree \
     --optimizer_name adam \
-    --n_epochs 500 \
-    --data_dir ./inputs/PPI/ \
-    --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name ggat2unet_with_gcn_ppi \
+    --n_epochs 100 \
+    --data_dir ./inputs/MUTAG/ \
+    --index_file_dir ./inputs/MUTAG/10fold_idx/ \
+    --name ggat2_with_gcn_mutag \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_ggat2unet_with_gcn_ppi \
+    --run_name debug_ggat2_with_gcn_mutag \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --pool_type SAGPool \
     --ggat_type GGAT2 \
-    --gnn_type GCN
+    --gnn_type GCN &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 4 \
     --verbose \
     --no_visdom_logger \
     --loss_name mce \
-    --task_type multi_label_node_classification \
-    --network_name UNet \
-    --dataset_name PPI \
+    --task_type graph_classification \
+    --network_name GGAT \
+    --dataset_name MUTAG \
     --train_transform_name indegree \
     --test_transform_name indegree \
     --optimizer_name adam \
-    --n_epochs 500 \
-    --data_dir ./inputs/PPI/ \
-    --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name unet_with_gat_sagpool_ppi \
+    --n_epochs 100 \
+    --data_dir ./inputs/MUTAG/ \
+    --index_file_dir ./inputs/MUTAG/10fold_idx/ \
+    --name ggat1_with_gat_mutag \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_unet_with_gat_sagpool_ppi \
+    --run_name debug_ggat1_with_gat_mutag \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --gnn_type GAT \
-    --pool_type SAGPool
+    --ggat_type GGAT1 \
+    --gnn_type GAT &
 sleep 10
 python3 gnn/train.py \
-    --gpu_ids 5 \
+    --gpu_ids 0,1,2,3,4,5,6,7 \
     --verbose \
     --no_visdom_logger \
     --loss_name mce \
-    --task_type multi_label_node_classification \
-    --network_name UNet \
-    --dataset_name PPI \
+    --task_type graph_classification \
+    --network_name GGAT \
+    --dataset_name MUTAG \
     --train_transform_name indegree \
     --test_transform_name indegree \
     --optimizer_name adam \
-    --n_epochs 500 \
-    --data_dir ./inputs/PPI/ \
-    --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name unet_with_gat_topkpool_ppi \
+    --n_epochs 100 \
+    --data_dir ./inputs/MUTAG/ \
+    --index_file_dir ./inputs/MUTAG/10fold_idx/ \
+    --name ggat1_with_gcn_mutag \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_unet_with_gat_topkpool_ppi \
+    --run_name debug_ggat1_with_gcn_mutag \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --gnn_type GAT \
-    --pool_type TopKPool
+    --ggat_type GGAT1 \
+    --gnn_type GCN &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 6 \
     --verbose \
     --no_visdom_logger \
     --loss_name mce \
-    --task_type multi_label_node_classification \
-    --network_name UNet \
-    --dataset_name PPI \
+    --task_type graph_classification \
+    --network_name GGAT \
+    --dataset_name MUTAG \
     --train_transform_name indegree \
     --test_transform_name indegree \
     --optimizer_name adam \
-    --n_epochs 500 \
-    --data_dir ./inputs/PPI/ \
-    --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name unet_with_gcn_sagpool_ppi \
+    --n_epochs 100 \
+    --data_dir ./inputs/MUTAG/ \
+    --index_file_dir ./inputs/MUTAG/10fold_idx/ \
+    --name ggat2_with_gat_mutag \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_unet_with_gcn_sagpool_ppi \
+    --run_name debug_ggat2_with_gat_mutag \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --gnn_type GCN \
-    --pool_type SAGPool
+    --ggat_type GGAT2 \
+    --gnn_type GAT &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 7 \
     --verbose \
     --no_visdom_logger \
     --loss_name mce \
-    --task_type multi_label_node_classification \
-    --network_name UNet \
-    --dataset_name PPI \
+    --task_type graph_classification \
+    --network_name GGAT \
+    --dataset_name MUTAG \
     --train_transform_name indegree \
     --test_transform_name indegree \
     --optimizer_name adam \
-    --n_epochs 500 \
-    --data_dir ./inputs/PPI/ \
-    --index_file_dir ./inputs/PPI/10fold_idx/ \
-    --name unet_with_gcn_topkpool_ppi \
+    --n_epochs 100 \
+    --data_dir ./inputs/MUTAG/ \
+    --index_file_dir ./inputs/MUTAG/10fold_idx/ \
+    --name ggat2_with_gcn_mutag \
     --save_freq 5 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name debug_unet_with_gcn_topkpool_ppi \
+    --run_name debug_ggat2_with_gcn_mutag \
     --lr 1e-3 \
     --beta1 0.9 \
     --beta2 0.999 \
-    --gnn_type GCN \
-    --pool_type TopKPool
+    --ggat_type GGAT2 \
+    --gnn_type GCN
--- scripts/run_ppi_unet_ggatunet.sh
+++ scripts/run_ppi_unet_ggatunet.sh
@@ -62,7 +62,7 @@ python3 gnn/train.py \
     --beta2 0.999 \
     --pool_type SAGPool \
     --ggat_type GGAT1 \
-    --gnn_type GCN
+    --gnn_type GCN &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 2 \
@@ -88,7 +88,7 @@ python3 gnn/train.py \
     --beta2 0.999 \
     --pool_type SAGPool \
     --ggat_type GGAT2 \
-    --gnn_type GAT
+    --gnn_type GAT &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 3 \
@@ -114,7 +114,7 @@ python3 gnn/train.py \
     --beta2 0.999 \
     --pool_type SAGPool \
     --ggat_type GGAT2 \
-    --gnn_type GCN
+    --gnn_type GCN &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 4 \
@@ -139,7 +139,7 @@ python3 gnn/train.py \
     --beta1 0.9 \
     --beta2 0.999 \
     --gnn_type GAT \
-    --pool_type SAGPool
+    --pool_type SAGPool &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 5 \
@@ -164,7 +164,7 @@ python3 gnn/train.py \
     --beta1 0.9 \
     --beta2 0.999 \
     --gnn_type GAT \
-    --pool_type TopKPool
+    --pool_type TopKPool &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 6 \
@@ -189,7 +189,7 @@ python3 gnn/train.py \
     --beta1 0.9 \
     --beta2 0.999 \
     --gnn_type GCN \
-    --pool_type SAGPool
+    --pool_type SAGPool &
 sleep 10
 python3 gnn/train.py \
     --gpu_ids 7 \
