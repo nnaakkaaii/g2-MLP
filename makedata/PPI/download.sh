f04f7e46125382d71979d4e1137327106e09780b
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2021-11-12 01:12:18 +0900

    add 5fold

--- makedata/PPI/download.sh
+++ makedata/PPI/download.sh
@@ -2,3 +2,6 @@ mkdir -p inputs/PPI
 wget "https://drive.google.com/uc?export=download&id=14nh4RizIXdCsdn3JvkMFUHIMD_ZFt4Ni" -O ./inputs/PPI/PPI.zip
 unzip ./inputs/PPI/PPI.zip -d ./inputs
 rm ./inputs/PPI/PPI.zip
+wget "https://drive.google.com/uc?export=download&id=136FI2v_6tMofUjnODFRGJBJiujzHeher" -O ./inputs/PPI/PPI.zip
+unzip ./inputs/PPI/PPI.zip -d ./inputs
+rm ./inputs/PPI/PPI.zip
