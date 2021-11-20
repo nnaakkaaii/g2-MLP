d3839fa3b11ab3063265a1208905ee981100c749
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2021-11-20 09:45:40 +0900

    sqliteが同時実行できない問題に対処

---
+++ scripts/nci1_gmlp.sh
@@ -0,0 +1,27 @@
+python3 gnn/train.py \
+    --gpu_ids 0,1,2,3,4,5,6,7 \
+    --batch_size 5096 \
+    --verbose \
+    --loss_name mce \
+    --network_name gmlp_graph_classification \
+    --dataset_name nci1 \
+    --train_transform_name indegree \
+    --val_transform_name indegree \
+    --optimizer_name adam \
+    --scheduler_name step \
+    --n_epochs 100 \
+    --data_dir ./inputs/NCI1/ \
+    --name 1118_tuning_gmlp_graph_classification_nci1 \
+    --save_freq 10 \
+    --save_dir ./checkpoints \
+    --mlflow_root_dir ./mlruns/ \
+    --run_name tuning_1118_0135 \
+    --lr 2.5e-3 \
+    --beta1 0.9 \
+    --beta2 0.9 \
+    --hidden_dim 32 \
+    --ffn_dim 512 \
+    --n_layers 4 \
+    --lr_decay_iters 300 \
+    --lr_decay_gamma 0.3 \
+    --prob_survival 1. &
