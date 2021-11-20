8f01b600edd7fe5575b969d22fbbd100c0cc2abb
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2021-11-20 10:16:18 +0900

    並列数のtuning

--- scripts/run_nci1_gmlp.sh
+++ scripts/run_nci1_gmlp.sh
@@ -1,3 +1,16 @@
+#!/bin/sh
+
+#PJM -L rscgrp=regular-a
+#PJM -L node=1
+#PJM -L elapse=48:00:00
+#PJM -g gs84
+#PJM -j
+
+module load cuda/11.1
+module load pytorch/1.8.1
+
+source /work/02/gs84/s84000/inductive_node_classification_models/.venv/bin/activate
+
 python3 gnn/train.py \
     --gpu_ids 0,1,2,3,4,5,6,7 \
     --batch_size 256 \
@@ -11,11 +24,11 @@ python3 gnn/train.py \
     --scheduler_name step \
     --n_epochs 100 \
     --data_dir ./inputs/NCI1/ \
-    --name 1118_tuning_gmlp_graph_classification_nci1 \
+    --name 1120_tuned_gmlp_nci1 \
     --save_freq 10 \
     --save_dir ./checkpoints \
     --mlflow_root_dir ./mlruns/ \
-    --run_name tuning_1118_0135 \
+    --run_name tuned \
     --lr 0.0025 \
     --beta1 0.9 \
     --beta2 0.9 \
